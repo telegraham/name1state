55c0cae59c77b2984b9fdc1353611c9e81dfca91
Graham Troyer-Joy <graham.troyer_joy@stitchfix.com>
2024-08-02 17:09:29 -0400

    Initial Commit

---
+++ name1state/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  name1state
+//
+//  Created by Graham Troyer-Joy on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ name1state/name1stateApp.swift
@@ -0,0 +1,17 @@
+//
+//  name1stateApp.swift
+//  name1state
+//
+//  Created by Graham Troyer-Joy on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct name1stateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
